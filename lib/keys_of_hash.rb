cedd5bc87714053b5d19ab1b96e8b72001176525
Daniel Shimon <danielzshimon@gmail.com>
2018-04-22 16:05:43 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of(*arguments)
     collect {|key, value| 
-      arguments.include (value) ? key : nil }
+      arguments.include (value) ? key : nil }.compact
   end
 end
    
